2f34b93b6eebb1d75a91eb5b012d4652895a6cac
Bharath <120463306+bharath8671@users.noreply.github.com>
2023-11-29 12:01:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.24.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   # Configuration options
 }
